2a6511749588d033e574132f3c3f468bb38fca5f
dev-pasa <dev5@uw.edu>
2018-10-23 15:17:44 -0700

    Checked Sam's codes for answers

--- js/app.js
+++ js/app.js
@@ -1,41 +1,124 @@
 'use strict';
 
-var mainHeader = document.getElementById('mainHeader');
+//++++++++++++++++++++++++++++++
+// SETTING UP GLOBAL DATA
+//++++++++++++++++++++++++++++++
 
-// Make a constructor function
-function Product(filepath, name, seen = 0) {
-  this.filepath = filepath;
-  this.name = name;
-  this.seen = seen;
-  Product.allProducts.push(this);
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+var allProducts = []; // This is the main array of objects
+var totalClicks = 0; // Tallies the 25 clicks
+
+// DOM access
+var container = document.getElementById('image_container');
+var left = document.getElementById('left');
+var center = document.getElementById('center');
+var right = document.getElementById('right');
+var productList = document.getElementById('productlist');
+var justViewed = [];
+
+//++++++++++++++++++++++++++++++
+// CONSTRUCTOR
+//++++++++++++++++++++++++++++++
+
+function Product(name) {
+  this.name = name; // this will be the alt/title value
+  this.filepath = `images/${name}.jpg`;
+  this.views = 0;
+  this.votes = 0;
+  allProducts.push(this);
 }
 
-//create array to hold  3 products
-var productIndex = [];
 
-//Product instances
-new Product('images/bag.jpg', 'bag');
-new Product('images/banana.jpg', 'banana');
-new Product('images/bathroom.jpg', 'bathroom');
-new Product('images/boots.jpg', 'boots');
-new Product('images/breakfast.jpg', 'breakfast');
-new Product('images/bubblegum.jpg', 'bubblegum');
-new Product('images/chair.jpg', 'chair');
-new Product('images/cthulhu.jpg', 'creature');
-new Product('images/dog-duck.jpg', 'dogduck');
-new Product('images/dragon.jpg', 'dragon');
-new Product('images/pen.jpg', 'pen');
-new Product('images/pet-sweep.jpg', 'petsweep');
-new Product('images/scissors.jpg', 'scissors');
-new Product('images/shark.jpg', 'shark');
-new Product('images/sweep.png', 'sweep');
-new Product('images/tauntaun.jpg', 'blanket');
-new Product('images/unicorn.jpg', 'unicorn');
-new Product('images/water-can.jpg', 'watercan');
-new Product('images/usb.gif', 'usb');
-new Product('images/wine-glass.jpg', 'wineglass');
+//++++++++++++++++++++++++++++++
+// INSTANCES
+//++++++++++++++++++++++++++++++
 
+for (var i = 0; i < names.length; i++) {
+  new Product(names[i]);
+}
 
-var product1 = Product.allProducts[0];
-var product2 = Product.allProducts[1];
-var product3 = Product.allProducts[2];
+//++++++++++++++++++++++++++++++
+// FUNCTION DECLARATIONS
+//++++++++++++++++++++++++++++++
+
+function makeRandom() {
+  return Math.floor(Math.random() * allProducts.length);
+}
+
+function makeThreeUnique() {
+  console.log(justViewed, 'just viewed in line 50');
+  var output = [];
+
+  var firstNum = makeRandom(); // makes first
+  while (justViewed.includes(firstNum)) {
+    firstNum = makeRandom(); // makes first again
+  }
+  output.push(firstNum);
+
+  output.push(makeRandom()); // makes second
+  while (output[0] === output[1]) {
+    console.log('duplicate detected on second');
+    output[1] = makeRandom();
+  }
+  output.push(makeRandom()); // makes third
+  while (output[0] === output[2] || output[1] === output[2]) {
+    console.log('duplicate detected on third');
+    output[2] = makeRandom();
+  }
+  justViewed = output;
+  return output;
+}
+
+function displayPics() {
+  var idx = makeThreeUnique();
+  console.log(idx, 'three new values in line 69 where we create them after a click');
+  allProducts[idx[0]].views++;
+  allProducts[idx[1]].views++;
+  allProducts[idx[2]].views++;
+  left.src = allProducts[idx[0]].filepath;
+  center.src = allProducts[idx[1]].filepath;
+  right.src = allProducts[idx[2]].filepath;
+  left.alt = allProducts[idx[0]].name;
+  center.alt = allProducts[idx[1]].name;
+  right.alt = allProducts[idx[2]].name;
+  left.title = allProducts[idx[0]].name;
+  center.title = allProducts[idx[1]].name;
+  right.title = allProducts[idx[2]].name;
+}
+
+function handleClick(event) {
+  if (event.target.id === 'image_container') {
+    return alert('Please click directly on an image');
+  }
+  totalClicks++;
+  // console.log(totalClicks, 'total clicks');
+
+  for(var i = 0; i < allProducts.length; i++) {
+    if (event.target.alt === allProducts[i].name){
+      allProducts[i].votes++;
+    }
+  }
+
+  if (totalClicks === 25) {
+    container.removeEventListener('click', handleClick);
+    return showList();
+  }
+  displayPics();
+}
+
+function showList() {
+  for (var i = 0; i < allProducts.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = `${allProducts[i].name} has ${allProducts[i].views} and views and ${allProducts[i].votes} votes`;
+    productList.appendChild(liEl);
+  }
+  
+}
+
+//++++++++++++++++++++++++++++++
+// CODE THAT EXECUTES ON PAGE LOAD
+//++++++++++++++++++++++++++++++
+
+displayPics();
+container.addEventListener('click', handleClick);
